8d7cdfd2f5fde55da16be75a90ab1e6fc5773c6c
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-06-01 20:12:45 -0300

    feat: layers and router

---
+++ src/database/PostDatabase.ts
@@ -0,0 +1,8 @@
+import { BaseDatabase } from "./BaseDatabase";
+
+export class PostDatabase extends BaseDatabase {
+    private static TABLE_POSTS="posts"
+    private static TABLE_LIKE_DISLIKE = "likes_dislikes"
+
+    //métodos db
+}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { userRouter } from './router/userRouter'
+import { postRouter } from './router/postRouter'
 
 dotenv.config()
 
@@ -10,7 +12,8 @@ app.use(cors())
 app.use(express.json())
 
 app.listen(process.env.PORT || 3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
+    console.log(`Servidor rodando na porta ${(process.env.PORT) || 3003}.`)
 })
 
-//app.use("/users", userRouter)
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
