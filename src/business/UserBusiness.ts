fb6aa820c4ec839e643bb82735e68d05fc32ecd7
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-06-02 22:44:49 -0300

    feat: endpoint login

--- src/business/UserBusiness.ts
+++ src/business/UserBusiness.ts
@@ -1,9 +1,12 @@
+import { BadRequestError } from './../errors/BadRequestError';
 import { HashManager } from './../services/HashManager';
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
 import { UserDatabase } from '../database/UserDatabase';
 import { SignupInputDTO, SignupOutputDTO } from '../dtos/user/signup.dto';
 import { TokenPayload, USER_ROLES, User } from '../models/User';
+import { LoginInputDTO, LoginOutputDTO } from '../dtos/user/login.dto';
+import { NotFoundError } from '../errors/NotFoundError';
 
 export class UserBusiness {
     constructor (
@@ -42,5 +45,44 @@ export class UserBusiness {
         }
 
         return output
+    }
+
+    public login = async (input: LoginInputDTO): Promise<LoginOutputDTO> => {
+        const {email, password} = input
+
+        const userDB = await this.userDatabase.findUserByEmail(email)
+
+        if(!userDB){
+            throw new BadRequestError("Email não encontrado.")
+        }
+
+        const user = new User (
+            userDB.id,
+            userDB.name,
+            userDB.email,
+            userDB.password,
+            userDB.role,
+            userDB.created_at
+        )
+
+        const isPasswordCorrect = await this.hashManager
+        .compare(password, user.getPassword())
+
+        if(!isPasswordCorrect){
+            throw new BadRequestError("E-mail ou senha incorreta.")
+        }
+
+        const payload: TokenPayload = {
+            id: user.getId(),
+            name: user.getName(),
+            role: user.getRole()
+        }
+
+        const output: LoginOutputDTO = {
+            token: this.tokenManager.createToken(payload)
+        }
+
+        return output
+
     }
 }
