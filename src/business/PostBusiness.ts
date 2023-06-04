50dae57c132b6616e356e6adb8fee8bbb613e04f
LudmilaASouza <ludmilasouz.a@hotmail.com>
2023-06-04 11:32:26 -0300

    feat: endpoint createPost

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -1,6 +1,9 @@
+import { CreatePostsInputDTO, createPostsOutputDTO } from './../dtos/post/createPosts.dto';
 import { PostDatabase } from "../database/PostDatabase";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
+import { UnauthorizedError } from '../errors/UnauthorizedError';
+import { Post } from '../models/Post';
 
 export class PostBusiness {
     constructor (
@@ -9,5 +12,32 @@ export class PostBusiness {
         private tokenManager: TokenManager
     ) {}
 
-    //endpoints
+    public createPost = async (input: CreatePostsInputDTO): Promise<createPostsOutputDTO> => {
+        
+        const {content, token} = input
+
+        const payload = this.tokenManager.getPayload(token)
+        if (!payload) {
+            throw new UnauthorizedError()
+        }
+        const id = this.idGenerator.generate()
+
+        const post = new Post(
+            id,
+            content,
+            0,
+            0,
+            new Date().toISOString(),
+            new Date().toISOString(),
+            payload.id,
+            payload.name
+        )
+
+        const postDB = post.toDBModel()
+        await this.postDatabase.insertPost(postDB)
+
+        const output: createPostsOutputDTO = undefined
+
+        return output
+    }
 }
--- src/database/PostDatabase.ts
+++ src/database/PostDatabase.ts
@@ -1,8 +1,13 @@
+import { PostDB } from './../models/Post';
 import { BaseDatabase } from "./BaseDatabase";
 
 export class PostDatabase extends BaseDatabase {
     private static TABLE_POSTS="posts"
     private static TABLE_LIKE_DISLIKE = "likes_dislikes"
 
-    //métodos db
+    public insertPost = async (postDB: PostDB): Promise<void> => {
+        await BaseDatabase
+        .connection(PostDatabase.TABLE_POSTS)
+        .insert(postDB)
+    }
 }
